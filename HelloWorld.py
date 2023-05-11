6c2efd79dd6703b6aebce69abede6c9f01ac3c8c
paulina232 <paulinamuar@gmail.com>
2023-05-11 10:41:14 -0500

    Ventana de actualizacion pablo 2023

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+ventana=Tk()
+
+ventana.title('Requerimientos 2023')
+
+
+etiqueta=Label(ventana,text='Hola soy un texto de requerimientos \nPablo 2023')
+
+etiqueta.pack()
+
+ventana.mainloop()
